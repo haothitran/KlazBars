be3ddb1d4aece327920ea9fde6978a68597b7b83
Hao Tran <account@haothitran.com>
2019-09-02 19:56:38 -0400

    Clean up

--- Modules/RangeIndicator.lua
+++ Modules/RangeIndicator.lua
@@ -7,7 +7,7 @@ if C.RangeIndicator ~= true then return end
 --------------------------------------------------------------------------------
 
 hooksecurefunc("ActionButton_OnEvent",function(self, event, ...)
-  if (event == "PLAYER_TARGET_CHANGED") then
+  if event == "PLAYER_TARGET_CHANGED" then
     self.newTimer = self.rangeTimer
   end
 end)
@@ -15,17 +15,19 @@ end)
 hooksecurefunc("ActionButton_UpdateUsable",function(self)
   local icon = _G[self:GetName().."Icon"]
   local valid = IsActionInRange(self.action)
-  if (valid == false) then
+
+  if valid == false then
     icon:SetVertexColor(1, 0, 0)
   end
 end)
 
 hooksecurefunc("ActionButton_OnUpdate",function(self, elapsed)
   local rangeTimer = self.newTimer
-  if (rangeTimer) then
+
+  if rangeTimer then
     rangeTimer = rangeTimer - elapsed
 
-    if (rangeTimer <= 0) then
+    if rangeTimer <= 0 then
       ActionButton_UpdateUsable(self)
       rangeTimer = TOOLTIP_UPDATE_TIME
     end
