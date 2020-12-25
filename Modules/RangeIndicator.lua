ba1e96001d655359f7f6d9bcdcfbfe47af1831f1
Hao Tran <account@haothitran.com>
2020-12-24 22:41:17 -0500

    Fix range indicator for Shadowlands

--- Modules/RangeIndicator.lua
+++ Modules/RangeIndicator.lua
@@ -2,32 +2,53 @@
 -- // MODULES / RANGE INDICATOR
 --------------------------------------------------------------------------------
 
-hooksecurefunc('ActionButton_OnEvent', function(self, event, ...)
-  if event == 'PLAYER_TARGET_CHANGED' then
-    self.newTimer = self.rangeTimer
+local function ActionBarActionButtonMixinHook_UpdateUsable(self, checksRange, inRange)
+  local icon = self.icon
+  local normalTexture = self.NormalTexture
+  if not normalTexture then
+    return
   end
-end)
 
-hooksecurefunc('ActionButton_UpdateUsable', function(self)
-  local icon = _G[self:GetName()..'Icon']
-  local valid = IsActionInRange(self.action)
+  local isUsable, notEnoughMana = IsUsableAction(self.action)
+  if isUsable then
+    icon:SetVertexColor(1, 1, 1)
+    normalTexture:SetVertexColor(1, 1, 1)
+  elseif notEnoughMana then
+    icon:SetVertexColor(.2, .2, 1)
+    normalTexture:SetVertexColor(.2, .2, 1)
+  else
+    icon:SetVertexColor(.5, .5, .5)
+    normalTexture:SetVertexColor(.5, .5, .5)
+  end
 
-  if valid == false then
+  if checksRange and not inRange then
     icon:SetVertexColor(1, 0, 0)
   end
-end)
+end
 
-hooksecurefunc('ActionButton_OnUpdate', function(self, elapsed)
-  local rangeTimer = self.newTimer
+hooksecurefunc("ActionButton_UpdateRangeIndicator", function(self, checksRange, inRange)
+  local icon = self.icon
 
-  if rangeTimer then
-    rangeTimer = rangeTimer - elapsed
-
-    if rangeTimer <= 0 then
-      ActionButton_UpdateUsable(self)
-      rangeTimer = TOOLTIP_UPDATE_TIME
-    end
-
-    self.newTimer = rangeTimer
+  if self.action then
+    ActionBarActionButtonMixinHook_UpdateUsable(self, checksRange, inRange)
   end
+
+  -- if self.HotKey:GetText() == RANGE_INDICATOR then
+  --   if checksRange then
+  --     self.HotKey:Show()
+  --     if inRange then
+  --       self.HotKey:SetVertexColor(.5, .5, .5, .5)
+  --     else
+  --       self.HotKey:SetVertexColor(1, .2, .2, .5)
+  --     end
+  --   else
+  --     self.HotKey:Hide()
+  --   end
+  -- else
+  --   if checksRange and not inRange then
+  --     self.HotKey:SetVertexColor(1, .2, .2, .5)
+  --   else
+  --     self.HotKey:SetVertexColor(.5, .5, .5, .5)
+  --   end
+  -- end
 end)
